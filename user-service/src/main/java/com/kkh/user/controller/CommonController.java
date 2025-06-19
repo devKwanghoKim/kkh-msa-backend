c7f4cffd3c8628cab73f4b0254a4eae265ac502e
kwangho <>
2025-06-19 16:16:47 +0900

    [Main] 초기프로젝트 세팅
    * gateway-service
      * gateway dependancy 수정
      * routes 추가
    * user-service
      * 테스트용 API 추가

---
+++ user-service/src/main/java/com/kkh/user/controller/CommonController.java
@@ -0,0 +1,15 @@
+package com.kkh.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1.0/users")
+public class CommonController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome";
+    }
+}
